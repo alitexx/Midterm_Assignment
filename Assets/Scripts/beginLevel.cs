995ba862205bf146e2d6f0734e2c7b80ceeb10e3
Katie T <kttackett123@gmail.com>
2022-10-19 16:43:04 -0400

    added beginLevel

---
+++ Assets/Scripts/beginLevel.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class beginLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //when level starts, flash a "level #" screen before it fades to black, giving the player
+        //control of their character
+    }
+}
