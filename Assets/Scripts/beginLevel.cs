a6479b8ade236ab418a14ea00dd0665f6a4d7070
Katie T <kttackett123@gmail.com>
2022-10-19 17:23:49 -0400

    added fade to black function to beginLevel

--- Assets/Scripts/beginLevel.cs
+++ Assets/Scripts/beginLevel.cs
@@ -5,10 +5,51 @@ using UnityEngine.UI;
 
 public class beginLevel : MonoBehaviour
 {
+
+    public GameObject blackOutSquare;
     // Start is called before the first frame update
     void Start()
     {
         //when level starts, flash a "level #" screen before it fades to black, giving the player
         //control of their character
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            StartCoroutine(FadeBlackOutSquare());
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            StartCoroutine(FadeBlackOutSquare(false));
+        }
+    }
+    public IEnumerator FadeBlackOutSquare(bool fadeToBlack = true, int fadeSpeed = 5)
+    {
+        Color objectColor = blackOutSquare.GetComponent<Image>().color;
+        float fadeAmount;
+
+        if (fadeToBlack)
+        {
+            while (blackOutSquare.GetComponent<Image>().color.a < 1)
+            {
+                fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                blackOutSquare.GetComponent<Image>().color = objectColor;
+                yield return null;
+            }
+        } else
+        {
+            while (blackOutSquare.GetComponent<Image>().color.a > 0)
+            {
+                fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                blackOutSquare.GetComponent<Image>().color = objectColor;
+                yield return null;
+            }
+        }
     }
 }
