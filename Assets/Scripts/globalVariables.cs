66c66e6f92d98bcbb79b1974b2b64738806adf63
Katie T <kttackett123@gmail.com>
2022-10-23 11:42:27 -0400

    added global variables

---
+++ Assets/Scripts/globalVariables.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class globalVariables : MonoBehaviour
+{
+    // used to determine what floor/level the player is on
+    public static int playerLevel;
+
+    // used to track the number of items the player has
+    public static int itemsCollected;
+}
